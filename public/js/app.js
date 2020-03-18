2cd9fa8291ca0ae08ca2aa4497f83b2dc1401744
Mayank <mayanklal08@icloud.com>
2020-03-18 21:19:14 +0530

    Making UI changes

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,13 @@
 const messageOne=document.querySelector('#message-1');
 const messageTwo=document.querySelector('#message-2');
+//const messageThree=document.querySelector('#message-3');
 const search=document.querySelector('#location');
 
 const weatherForm=document.querySelector('#form').addEventListener('submit',(e)=>{
     e.preventDefault();
     messageOne.textContent="Loading..";
     messageTwo.textContent="";
+    //messageThree.textContent="";
     const location=search.value;
     fetch(`/weather?location=${location}`)
     .then((response)=>{
@@ -15,9 +17,11 @@ const weatherForm=document.querySelector('#form').addEventListener('submit',(e)=
         if(data.error){
             messageOne.textContent=data.error;
             messageTwo.textContent="";
+            //messageThree.textContent="";
         }else{
             messageOne.textContent=data.location.location;
-            messageTwo.textContent= data.TemperatureDaily.data[0].summary+". It is currently "+data.TemperatureData.temperature+" degrees out.";    
+            messageTwo.textContent= data.TemperatureDaily.data[0].summary+". It is currently "+data.TemperatureData.temperature+" degrees out.";  
+            //messageThree.textContent=data.TemperatureDaily.data[0].temperatureHigh;  
         }      
     })
 
